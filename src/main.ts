953da533e4bd00d14ce7c8f782645aa12e7f70b4
Tam Dinh <nguyenductam98765@gmail.com>
2026-09-18 13:34:00 +0700

    feat: add global exception filter and response transformer

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './interceptor/transform.interceptor';
+import { CatchEverythingFilter } from './filter/catch.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-
+  app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalPipes(new ValidationPipe());
-
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CatchEverythingFilter(httpAdapter));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
